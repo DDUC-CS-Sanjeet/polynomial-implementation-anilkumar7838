2c61a8dce2bcaa4728f8d60081ff9c22f6c28358
anilkumar7838 <59726944+anilkumar7838@users.noreply.github.com>
2020-01-27 20:37:04 +0530

    Update Polynomial.cpp

--- Polynomial.cpp
+++ Polynomial.cpp
@@ -1,55 +1,141 @@
 #include<iostream>
 using namespace std;
-
-class Polynomial
+class polynomial
 {
-  private:
-    // Variables to store information about polynomial
-  public:
-    Polynomial()
+private:
+    int degree;
+    int a[20];
+public:
+    polynomial()
     {
-      // Behavior of default constructor
+        degree=0;
     }
-
-    Polynomial(int deg)
+    polynomial(int deg)
     {
-      // Behavior of constructor with arguments
+        degree=deg;
     }
-  
-    ~Polynomial()
+    ~polynomial()
     {
-      // Behavior of destructor
+        //cout<<"this is destructor";
     }
-  
-    //  Overload copy constructor, +, - and = operators
-  
     void storePolynomial()
     {
-      //  Code to enter and store polynomial
+        int i;
+        cout<<"enter the terms:"<<endl;
+        for(i=degree; i>=0; i--)
+        {
+            cin>>a[i];
+        }
     }
     void display()
     {
-      //  Code to print the polynomial in readable format
+        int i;
+        for(i=degree; i>=0; i--)
+        {
+            if(i==0)
+            {
+                cout<<a[i];
+            }
+            else if(i==1)
+            {
+                cout<<a[i]<<"x"<<" + ";
+            }
+            else
+            {
+                cout<<a[i]<<"x^"<<i<<" + ";
+            }
+        }
     }
-  
+    polynomial operator+(polynomial ob)
+    {
+        polynomial temp;
+        int maxi=max(this->degree,ob.degree);
+        int mini=min(this->degree,ob.degree);
+        temp.degree=maxi;
+        int i;
+        for (i=0; i<=mini; i++)
+        {
+            temp.a[i]=this->a[i]+ob.a[i];
+            cout<<temp.a[i]<<endl;
+        }
+        for(i=mini+1; i<=maxi; i++)
+        {
+            if(this->degree>ob.degree)
+            {
+                temp.a[i]=this->a[i];
+            }
+            else
+            {
+                temp.a[i]=ob.a[i];
+            }
+        }
+        return temp;
+    }
+    polynomial operator-(polynomial ob)
+    {
+        polynomial temp;
+        int maxi=max(this->degree,ob.degree);
+        int mini=min(this->degree,ob.degree);
+        temp.degree=maxi;
+        int i;
+        for (i=0; i<=mini; i++)
+        {
+            temp.a[i]=this->a[i]-ob.a[i];
+            cout<<temp.a[i]<<endl;
+        }
+        for(i=mini+1; i<=maxi; i++)
+        {
+            if(this->degree>ob.degree)
+            {
+                temp.a[i]=this->a[i];
+            }
+            else
+            {
+                temp.a[i]=-ob.a[i];
+            }
+        }
+        return temp;
+    }
+    polynomial operator=(polynomial firstPolynomial)
+{
+    polynomial temp;
+    int i;
+    temp.degree=firstPolynomial.degree;
+
+    for(i=0;i<=firstPolynomial.degree;i++){
+        temp.a[i]=firstPolynomial.a[i];
+    }
+
+    return temp;
+
 }
 
-int main()
-{
-  int degFirst, degSecond;
-  // Ask user to input the values of degFirst and degSecond 
-  Polynomial firstPolynomial(degFirst);
-  Polynomial secondPolynomial(degSecond);
-  Polynomial thirdPolynomial;
-  
-  firstPolynomial.storePolynomial();
-  secondPolynomial.storePolynomial();
-  
-  thirdPolynomial=firstPolynomial+secondPolynomial;
-  Polynomial fourthPolynomial=firstPolynomial-secondPolynomial;
-  
-  firstPolynomial.display();
-  secondPolynomial.display();
-  thirdPolynomial.display();
-  fourthPolynomial.display();
-}
+    };
+    int main()
+    {
+        int degFirst,degSecond;
+        cout<<"enter degree1:";
+        cin>>degFirst;
+        cout<<"enter degree2:";
+        cin>>degSecond;
+        polynomial firstPolynomial(degFirst);
+        polynomial secondPolynomial(degSecond);
+
+        firstPolynomial.storePolynomial();
+        secondPolynomial.storePolynomial();
+
+        polynomial thirdPolynomial=firstPolynomial+secondPolynomial;
+        polynomial fourthPolynomial=firstPolynomial-secondPolynomial;
+        cout<<"\n\nthe first polynomial is\n";
+        firstPolynomial.display();
+        cout<<"\n\nthe second polynomial is\n";
+        secondPolynomial.display();
+        cout<<"\n\nthe sum is\n";
+        thirdPolynomial.display();
+        cout<<"\n\nthe difference is\n";
+        fourthPolynomial.display();
+        polynomial fifthPolynomial=firstPolynomial;
+        cout<<"\n\nafter use of  assignment operator\n";
+        fifthPolynomial.display();
+        return 0;
+    }
